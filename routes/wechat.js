36088ca07f5a9ece6e537aa8af38f624fdb72d24
Edison Yin <1984265648@qq.com>
2020-02-24 09:05:04 +0800

    add config

--- routes/wechat.js
+++ routes/wechat.js
@@ -21,42 +21,38 @@ let testConfig = {
 
 var token = "wechatexpress"; //此处是我们自定义的token，需与准备填写到微信配置上的一致！
 
-// router.get('/', function (req, res, next) {
-//     logger.info(`get wechat api request, headers is ${JSON.stringify(req.headers)}, request query is ${JSON.stringify(req.query)}`)
-//     var signature = req.query.signature
-//     var timestamp = req.query.timestamp
-//     var nonce = req.query.nonce
-//     var echostr = req.query.echostr
+router.get('/', function (req, res, next) {
+    logger.info(`get wechat api request, headers is ${JSON.stringify(req.headers)}, request query is ${JSON.stringify(req.query)}`)
+    var signature = req.query.signature
+    var timestamp = req.query.timestamp
+    var nonce = req.query.nonce
+    var echostr = req.query.echostr
 
-//     /*  加密/校验流程如下： */
-//     //1. 将token、timestamp、nonce三个参数进行字典序排序
-//     var array = new Array(token, timestamp, nonce);
-//     array.sort();
-//     var str = array.toString().replace(/,/g, "")
+    /*  加密/校验流程如下： */
+    //1. 将token、timestamp、nonce三个参数进行字典序排序
+    var array = new Array(token, timestamp, nonce);
+    array.sort();
+    var str = array.toString().replace(/,/g, "")
 
-//     //2. 将三个参数字符串拼接成一个字符串进行sha1加密
-//     var sha1Code = crypto.createHash("sha1")
-//     var code = sha1Code.update(str, 'utf-8').digest("hex");
+    //2. 将三个参数字符串拼接成一个字符串进行sha1加密
+    var sha1Code = crypto.createHash("sha1")
+    var code = sha1Code.update(str, 'utf-8').digest("hex");
 
-//     //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-//     if (code === signature) {
-//         res.send(echostr)
-//     } else {
-//         res.send("error");
-//     }
-// });
-router.use(express.query());
-
-router.use('/', wechat(testConfig, function (req, res, next) {
-    console.log(req.weixin);
-    logger.info(`get request from req ${req}`)
-    var message = req.weixin;
-    //文本
-    if (message.Content === '1') {
-        res.reply('hello');
+    //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+    if (code === signature) {
+        res.send(echostr)
     } else {
-        res.reply('hello world!');
+        res.send("error");
     }
-}));
+});
+
+router.post('/', function(req, res, next) {
+    // 获取参数
+    var query = req.body
+    logger.console.error("post请求：参数", query)
+    res.reply('hello world!')
+    res.send('hello , world')
+});
+router.use(express.query());
 
 module.exports = router;
